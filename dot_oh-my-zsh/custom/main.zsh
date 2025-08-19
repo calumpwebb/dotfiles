35b66961ecbafad93b0b066045ef8593f9152894
Calum Webb <calumpeterwebb@icloud.com>
2025-08-18 22:25:46 -0700

    brewfile added

--- dot_oh-my-zsh/custom/main.zsh
+++ dot_oh-my-zsh/custom/main.zsh
@@ -8,3 +8,16 @@ for file in $ZSH_CUSTOM/aliases/*.zsh(.N); do
   source "$file"
 done
 
+# wraps brew so that it auto updates the $HOME/Brewfile when we install
+# new packages
+BREWFILE=$HOME/.brewfile
+brew() {
+  if [[ "$1" == "install" ]]; then
+    # run the real brew first
+    command brew "$@"
+    # then update your Brewfile with the new package
+    command brew bundle dump --file="$BREWFILE" --force --describe --all
+  else
+    command brew "$@"
+  fi
+}
