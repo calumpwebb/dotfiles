3e1233b0e1b23a1d07a794b2210a406f0c46e861
Calum Webb <calumpeterwebb@icloud.com>
2025-08-18 23:21:42 -0700

    updated for brew uninstall too

--- dot_oh-my-zsh/custom/main.zsh
+++ dot_oh-my-zsh/custom/main.zsh
@@ -10,14 +10,12 @@ done
 
 # wrap brew so that it auto updates the $HOME/Brewfile
 # when we install new packages
-
 export HOMEBREW_BUNDLE_FILE_GLOBAL="$HOME/.Brewfile"
-
 brew() {
-  if [[ "$1" == "install" ]]; then
+  if [[ "$1" == "install" || "$1" == "uninstall" ]]; then
     # run the real brew first
     command brew "$@"
-    # then update your Brewfile with the new package
+    # then update your Brewfile
     command brew bundle dump --file="$HOMEBREW_BUNDLE_FILE_GLOBAL" --force --describe --all
     echo "Updated Brewfile=$HOMEBREW_BUNDLE_FILE_GLOBAL"
   else
